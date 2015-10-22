9b9689180d85ddf66a7c8d9b6b0762728b876ff4
Ramon Carvalho Maciel <ramongtx@gmail.com>
2015-10-22 13:34:57 -0200

    Adapting example project

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var cool = require('cool-ascii-faces')
 var express = require('express');
 var app = express();
+
+var cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: "session",
+  keys: ['key0','key1']
+}));
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,17 +21,37 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  console.log('loginData',req.session.loginData)
+  res.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-    result += cool();
-  response.send(result);
+app.get('/profile', isLoggedIn, function(req, res) {
+  res.render('pages/profile.ejs', {
+    name : "Ramon"
+  });
 });
+
+app.post('/login', function(req, res) {
+  req.session.loginData = req.body;
+  console.log('loginData',req.session.loginData)
+  res.redirect('/profile');
+  console.log('page rendered')
+});
+
+// route for logging out
+app.get('/logout', function(req, res) {
+    if (req.session.loginData) req.session.loginData = {};
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.session && req.session.loginData && req.session.loginData.event) {
+      return next();
+    } else {
+      res.redirect('/');
+    }
+}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
